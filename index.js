114ce5c95106969b6c039a51c50a0d0ed19ca716
jh <parkje09270@gmail.com>
2021-07-13 15:27:52 +0900

    노드 리액트 기초 강의 #8 Nodemon 설치

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+//bodyParser option
+//application/x-www-form-urlencoded 데이터를 분석해서 가져올 수 있게 해줌
+app.use(bodyParser.urlencoded({extended: true}));
+//application.json 데이터를 분석해서 가져올 수 있게 해줌
+app.use(bodyParser.json());
+
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://junghyun:wjdgus002@boiler-plate.eijjc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
@@ -10,6 +19,22 @@ mongoose.connect('mongodb+srv://junghyun:wjdgus002@boiler-plate.eijjc.mongodb.ne
 
 app.get('/', (req, res) => {
   res.send('Hello World! 안녕하세요')
+})
+
+//route 만들기
+//bodyParser 를 통해 client 에서 보내주는 회원가입 정보들을 가져오면, 
+//그것들을 데이터베이스에 넣어준다.
+app.post('/register', (req, res) => {
+  const user = new User(req.body)
+  user.save((err, doc) => {
+    //에러 발생 시 client 에게 에러가 있다고 json 형식으로 전달
+    if (err) return res.json({ success: false, err})
+    
+    //성공 시
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
---
+++ summary.md
@@ -0,0 +1,9 @@
+### client, server 통신하는 법
+- Body-parser 를 이용해서 client 에서 보내주는 데이터를 받을 수 있다. 
+- postman 설치 후, run -> postman 에서 post 통신
+
+### Nodemon
+- 코드의 변경사항을 바로 적용할 수 있도록 처리
+- 시작할 때 nodemon 으로 시작하기 위해 script 하나를 추가
+    + "backend": "nodemon index.js"
+    + 서버 시작 방법 : npm run backend
